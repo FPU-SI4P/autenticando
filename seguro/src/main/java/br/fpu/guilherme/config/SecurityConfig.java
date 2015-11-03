246b22ff9c46f39cac5df5e62684c1ba5ceb2a5b
guilherme <guilherme@Siberia>
2015-11-03 18:43:20 -0200

    autenticação simples

---
+++ seguro/src/main/java/br/fpu/guilherme/config/SecurityConfig.java
@@ -0,0 +1,37 @@
+package br.fpu.guilherme.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configurers.provisioning.InMemoryUserDetailsManagerConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+
+@Configuration
+@EnableWebMvcSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.antMatchers("/", "/home")
+				.permitAll()
+			.anyRequest()
+				.authenticated()
+			.and()
+				.formLogin()
+					.loginPage("/login")
+					.permitAll()
+			.and()
+				.logout()
+				.permitAll();
+	}
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		InMemoryUserDetailsManagerConfigurer<AuthenticationManagerBuilder> inMemoryAuthentication = auth.inMemoryAuthentication();
+			inMemoryAuthentication.withUser("admin").password("adminadmin").roles("USER");
+			inMemoryAuthentication.withUser("gui").password("pass").roles("USER");
+	}
+
+}
